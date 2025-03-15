d769d4a0b234ba57a4134a8b7ae7e0803130adae
oliwiawaszczuk <oliwiawaszczuk12321@gmail.com>
2025-03-15 22:32:10 +0100

    main.c

--- Sort/src/bubble_sort.c
+++ Sort/src/bubble_sort.c
@@ -1,13 +1,24 @@
+#include <stdio.h>
+
 #include "sort.h"
 
 void bubble_sort(int *array, int size) {
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                int temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
+
+
+    printf("\n");
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
             }
         }
+    printf("Tablica pp posortowaniem : ");
+        for (int i = 0; i < size; i++) {
+            printf("%d ", array[i]);
+
+        }
     }
-}
--- Sort/src/main.c
+++ Sort/src/main.c
@@ -1,3 +1,34 @@
+#include <stdio.h>
+#include "sort.h"
+
 int main() {
+    int n,temp,wybor;
+
+    printf("Enter the number of elements: ");
+    scanf("%d",&n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        printf("Element %d: ", i);
+        scanf("%d: ", &temp);
+        arr[i] = temp;
+    }
+    printf("1-Babelkowe \n 2-Quicksort \n 3-Insertionsort \n 4-Mergesort \n Wybierz sposob sortowania \n");
+    scanf("%d", &wybor);
+    switch (wybor) {
+        case 1: bubble_sort(arr,n);
+        break;
+        case 2: quick_sort(arr,n);
+        break;
+        case 3: insertion_sort(arr,n);
+        break;
+        case 4:
+            merge_sort(arr,n);
+            break;
+        default:
+            printf("Wrong wybor \n");
+    }
+
+
     return 0;
 }
